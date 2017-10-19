92bb176d185f4cf69da799487cdb09bef555cec6
Tom <tom.wilczak@gmail.com>
2017-10-19 16:04:36 -0500

    Add ngrx modules to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { StoreRouterConnectingModule, routerReducer, RouterStateSerializer } from '@ngrx/router-store';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { MailboxComponent } from './mailbox/mailbox.component';
 import { MailboxControlsComponent } from './mailbox/mailbox-controls/mailbox-controls.component';
+import { CustomRouterStateSerializer} from './utils';
+
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -14,9 +23,16 @@ import { MailboxControlsComponent } from './mailbox/mailbox-controls/mailbox-con
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpModule,
+    AppRoutingModule,
+    StoreModule.forRoot({ routerReducer: routerReducer }),
+    StoreRouterConnectingModule,
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+    EffectsModule.forRoot([]),
   ],
-  providers: [],
+  providers: [
+    {provide: RouterStateSerializer, useClass: CustomRouterStateSerializer}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
